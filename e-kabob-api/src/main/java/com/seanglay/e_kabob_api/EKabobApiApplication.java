22e03b555757dddba3f0ba5840b07583ad0da09d
SEANGLAY <devolutionary.wizard@gmail.com>
2025-02-02 18:25:23 +0700

    init project

---
+++ e-kabob-api/src/main/java/com/seanglay/e_kabob_api/EKabobApiApplication.java
@@ -0,0 +1,13 @@
+package com.seanglay.e_kabob_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKabobApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EKabobApiApplication.class, args);
+	}
+
+}
